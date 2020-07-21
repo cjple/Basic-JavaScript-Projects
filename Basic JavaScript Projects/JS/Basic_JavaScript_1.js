7444960640826d755848e2186f475be96355f159
Albert Lee <45klee@gmail.com>
2020-07-20 19:26:44 -0700

    first js commit

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,31 @@
+window.alert("Hello Family") //Makes a popup window//
+
+document.write("Albert said \"We live in Portland." //Concatenated Strings//
++ " We have a cat\"")
+
+
+var X = "Albert's" + " House" //Concatenated strings as variable//
+document.write(X)
+
+var Y = "House"
+var Z = "Lee"
+document.write(Y+Z)
+
+
+var Family = "Lees", Dad = "John", Mom = "Anne", Daughter = "Grace", Son = "Albert"; //Defines variables and gives it string value//
+var Family = Family.fontcolor("blue") //Changes var Family to color blue//
+document.write(Family); //Writes var Family//
+
+var Dad = Dad.fontcolor("red")
+document.write(Dad);
+
+var Mom = Mom.fontcolor("green")
+document.write(Mom);
+
+var Daughter = Daughter.fontcolor("yellow")
+document.write(Daughter);
+
+var Son = Son.fontcolor("blue")
+document.write(Son);
+
+Document.write(3 + 3);
